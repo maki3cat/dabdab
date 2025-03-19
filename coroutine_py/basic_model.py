78af4093bc9174b164e35fddb9d15c00813fa653
maki.xie <makixie1@outlook.com>
2025-03-18 22:44:45 -0400

    find a bug

--- coroutine_py/basic_model.py
+++ coroutine_py/basic_model.py
@@ -54,10 +54,13 @@ class _Eventloop:
 
     def run(self):
         while self.ready:
-            coro = self.ready.pop()
-            future, func, args, kwargs = coro
+            future, func, args, kwargs = self.ready.pop()
             try:
-                _ = func(*args, **kwargs)
+                next_future = func(*args, **kwargs)
+                # TODO: here has a core problem
+                # func is not finished, we need to wait func to 
+                # finish and then call this future's callback again
+                # or our tasks are lost
             except _CoroutineStop as es:
                 future.set_result(es.get_value())
 
@@ -82,7 +85,7 @@ class _CoroutineContext:
         self.coroutine = func
 
     def resume(self, future: _Future):
-        print(f"resume to state {self.state}")
+        print(f"RESUME -- to {self.coroutine.__name__} state {self.state}")
         self.history[self.current_coro] = future.result
         if self.coroutine != None:
             self.coroutine(self)
@@ -97,9 +100,10 @@ def coroutine_simple_1(message:str):
             ftr1 = _Future()
             ftr1.set_callback(con.resume)
             loop.call_now(ftr1, coroutine_simple_2, "call from simple 1--first time")
+            print(f"YIELD -- from _coroutine_simple_1 state {con.state}")
             return ftr1
         if con.state == 1:
-            # TODO: shall raise stop iteration
+            print("_coroutine_simple_1 completes")
             raise _CoroutineStop("result_activity_1")
 
     con = _CoroutineContext()
@@ -135,14 +139,14 @@ def common_workflow(name: str):
             # save the state of caller coroutine
             con.state = 2
             con.current_coro = "coroutine_simple_1"
+
             future = _Future()
-            
             # maki: very important to add resume to future when await on some future
             future.set_callback(con.resume)
             # maki: key to register the coroutine and future together
             loop.call_now(future, coroutine_simple_1, "first message")
-            print(f"yield from state {con.state}")
-            return
+            print(f"YIELD --- _common_workflow from state {con.state}")
+            return future
 
         # state after the coroutine call
         if con.state == 2:
@@ -150,10 +154,11 @@ def common_workflow(name: str):
             con.state = 3
             con.current_coro = "coroutine_simple_2"
             message = con.history.get("coroutine_simple_1")
-            future = _Future(callback=con.resume)
-            loop.call_now(future, coroutine_simple_2, message)
-            print(f"yield from state {con.state}")
-            return
+
+            ftr2 = _Future(callback=con.resume)
+            loop.call_now(ftr2, coroutine_simple_2, message)
+            print(f"YIELD --- _common_workflow from state {con.state}")
+            return future
 
         calculation_result = con.history.get("calculation_result")
         final_result = calculation_result + 100
@@ -167,5 +172,5 @@ def common_workflow(name: str):
 if __name__ == "__main__":
     c_runtime = _Eventloop.get_current_eventloop()
     c_runtime.call_now(None, common_workflow, "GLOVER")
-    c_runtime.call_now(None, common_workflow, "REBETA")
+    # c_runtime.call_now(None, common_workflow, "REBETA")
     c_runtime.run()
